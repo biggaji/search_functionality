d7f54f7467107ec617ef90b4d3fbf0638a69cd2e
Tobi Ajibade <herityjohnny14>
2021-02-11 15:28:56 -0800

    fixing deployment error

--- configs/db.js
+++ configs/db.js
@@ -7,7 +7,8 @@ const dbConnection = new Pool({
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    url: process.env.URL
 });
 
 module.exports = {
