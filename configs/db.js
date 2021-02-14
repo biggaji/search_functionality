910245663d7a9d0ac0d8423ce4a4f0dc09b4f605
Tobi Ajibade <herityjohnny14>
2021-02-14 00:23:28 -0800

    clean up for production

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg");
 
 // Instanciate a new pool connection
-
+// for production use this connection
+// while for development use the other configs
 const dbConnection = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
